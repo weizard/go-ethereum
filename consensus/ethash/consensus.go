b70629b6ae82c5b0533af792a355afcd8242a575
Jay Hsu <jay@dinngo.co>
2020-11-16 12:05:31 +0800

    [Update] update the diff for dinngo testing

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -315,19 +315,20 @@ func (ethash *Ethash) CalcDifficulty(chain consensus.ChainHeaderReader, time uin
 // the difficulty that a new block should have when created at time
 // given the parent block's time and difficulty.
 func CalcDifficulty(config *params.ChainConfig, time uint64, parent *types.Header) *big.Int {
-	next := new(big.Int).Add(parent.Number, big1)
-	switch {
-	case config.IsMuirGlacier(next):
-		return calcDifficultyEip2384(time, parent)
-	case config.IsConstantinople(next):
-		return calcDifficultyConstantinople(time, parent)
-	case config.IsByzantium(next):
-		return calcDifficultyByzantium(time, parent)
-	case config.IsHomestead(next):
-		return calcDifficultyHomestead(time, parent)
-	default:
-		return calcDifficultyFrontier(time, parent)
-	}
+	return big.NewInt(250000)
+	// next := new(big.Int).Add(parent.Number, big1)
+	// switch {
+	// case config.IsMuirGlacier(next):
+	// 	return calcDifficultyEip2384(time, parent)
+	// case config.IsConstantinople(next):
+	// 	return calcDifficultyConstantinople(time, parent)
+	// case config.IsByzantium(next):
+	// 	return calcDifficultyByzantium(time, parent)
+	// case config.IsHomestead(next):
+	// 	return calcDifficultyHomestead(time, parent)
+	// default:
+	// 	return calcDifficultyFrontier(time, parent)
+	// }
 }
 
 // Some weird constants to avoid constant memory allocs for them.
